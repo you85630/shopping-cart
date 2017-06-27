40bdbc03073f8a0fbd95511856fa5adac024e4fa
You <you85630@foxmail.com>
2017-06-27 16:03:25 +0800

    修改

--- src/components/pages/Hello.vue
+++ src/components/pages/Hello.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
